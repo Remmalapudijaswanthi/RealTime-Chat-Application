468312e72ebb367426dc380003f292488e103a83
Remmalapudijaswanthi <jaswanthiremmalapudi07@gmail.com>
2026-03-23 18:48:39 +0530

    Revert "Add 127.0.0.1 to CORS origins"
    This reverts commit 869da6136e9fcaec0616c77354d6d080e8b035d6.

--- server/server.js
+++ server/server.js
@@ -61,7 +61,6 @@ const io = new Server(server, {
 // Middleware
 const allowedOrigins = [
   'http://localhost:3000',
-  'http://127.0.0.1:3000',
   'http://localhost:5173',
   process.env.CLIENT_URL,
 ].filter(Boolean)
